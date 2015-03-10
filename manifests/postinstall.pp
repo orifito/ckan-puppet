c90dc1558751df1dc86724ad987666f4d6a6a83f
Oriol Fitó <oriol@akvo.org>
2015-03-10 15:31:44 +0100

    [akvo/akvo-provisioning#135] Updating command to set datastore permissions

--- manifests/postinstall.pp
+++ manifests/postinstall.pp
@@ -13,9 +13,8 @@ class ckan::postinstall {
   }
 
   check_run::task { 'set_database_perms':
-    exec_command => "/usr/bin/python /usr/lib/ckan/default/src/ckan/ckanext/\
-datastore/bin/datastore_setup.py ckan_default datastore_default ckan_default\
- ckan_default datastore_default -p postgres",
+    cwd          => "/var/lib/postgresql",
+    exec_command => "/usr/bin/ckan datastore set-permissions | sudo -u postgres psql --set ON_ERROR_STOP=1",
     require      => Check_run::Task['init_db'],
   }
 }
